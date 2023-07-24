ba1152c0ca4c11500d2009982f542fe6fc2d5223
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-07-24 17:46:49 -0300

    Add `Tests` project

---
+++ ricaun.Nuke.PackageBuilder.Tests/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ricaun.Nuke.PackageBuilder.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
